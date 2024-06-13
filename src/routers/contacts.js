4c7b39ae8ba10613084ee41cb31d1199f3e86a0a
RuslanGon <ruslan441006@gmail.com>
2024-06-13 17:33:36 +0300

    wer

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,11 +4,12 @@ import { createContactController, deleteContactController, getContactByIdControl
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
-const router = Router();
+const contactsRouter = Router();
 
-router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts',  validateBody(createContactSchema),ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-export default router;
+contactsRouter.get('/', ctrlWrapper(getContactsController));
+contactsRouter.get('/:contactId', ctrlWrapper(getContactByIdController));
+contactsRouter.post('/',  validateBody(createContactSchema),ctrlWrapper(createContactController));
+contactsRouter.patch('/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+contactsRouter.delete('/:contactId', ctrlWrapper(deleteContactController));
+
+export default contactsRouter;
