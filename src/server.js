7975e074a4c16a759eaa4e22c675ac905ac9ae58
RuslanGon <ruslan441006@gmail.com>
2024-06-13 17:38:52 +0300

    wer

--- src/server.js
+++ src/server.js
@@ -2,28 +2,33 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
-import contactsRouter from './routers/contacts.js';
+import rootRouter from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3001'));
 
 export const setupServer = () => {
+
     const app = express();
-    app.use(express.json());
+
     app.use(cors());
 
-    app.use(
-        pino({
-            transport: {
-                target: 'pino-pretty',
-            },
-        }),
-    );
+    app.use(cookieParser());
+
+    app.use(express.json());
+
+    app.use(pino({
+        transport: {
+            target: 'pino-pretty',
+        },
+    })
+        ,);
+
+    app.use(rootRouter);
 
 
-    app.use(contactsRouter);
 
     app.use('*', notFoundHandler);
 
@@ -32,4 +37,5 @@ export const setupServer = () => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
+
 };
